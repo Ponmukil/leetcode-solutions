73d51445c010976e1e76f635dc6df940caf530d7
Ponmukil <ponmukil49@gmail.com>
2026-02-20 21:36:43 +0530

    Time: 1 ms (68.90%) | Memory: 43 MB (72.63%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.java
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.java
@@ -5,7 +5,6 @@ class Solution {
         for(int num : nums){
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        System.out.println(map);
         for(int i = 0; i < nums.length; i++){
             if(map.get(nums[i]) == 1) sum += nums[i];
         }
