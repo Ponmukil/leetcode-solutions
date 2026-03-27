264f89942bf6c477aa6c9660932b06a14eb0c22f
Ponmukil <ponmukil49@gmail.com>
2026-03-27 21:30:50 +0530

    Time: 4 ms (44.89%) | Memory: 51.6 MB (82.85%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -2,13 +2,7 @@ class Solution {
     public void findIsland(char[][] grid, int row, int col, int[][] visited){
         int rows = grid.length, cols = grid[0].length;
         if(row >= rows || col >= cols || row < 0 || col < 0) return;
-        
-        // System.out.println(row + " " + col);
-        // 
-        // System.out.println("Helo");
-        // 
         if(grid[row][col] == '1' && visited[row][col]  == 0){
-            System.out.println(row + " "+ col);
             visited[row][col] = 1;
             findIsland(grid, row-1, col, visited);
             findIsland(grid, row, col-1, visited);
@@ -24,7 +18,6 @@ class Solution {
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < cols; j++){
                 if(grid[i][j] == '1' && visited[i][j] == 0){
-                    // System.out.println(i + " " + j);
                     cnt++;
                     findIsland(grid, i, j, visited);
                 }
