078791ae3b5db00ee52ed25fac9db044b2d1dbe4
Ponmukil <ponmukil49@gmail.com>
2026-03-30 21:49:10 +0530

    Time: 8 ms (2.71%) | Memory: 43.4 MB (6.32%) - LeetSync

---
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+
+        int i = 0, j = 0;
+        while(i < v1.length && j < v2.length){
+            int t1 = Integer.parseInt(v1[i]);
+            int t2 = Integer.parseInt(v2[j]);
+            System.out.println(t1 + " " +t2);
+            if(t1 < t2) return -1;
+            else if(t1 > t2) return 1;
+            i++;
+            j++;
+        }
+        while(i < v1.length){
+            if(Integer.parseInt(v1[i]) > 0) return 1;
+            i++;
+        }
+        while(j < v2.length){
+            if(Integer.parseInt(v2[j]) > 0) return -1;
+            j++;
+        }
+        return 0;
+
+    }
+}
