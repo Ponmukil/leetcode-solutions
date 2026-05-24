93d6c2574093192a36d48155dc5e9568d9cf5357
Ponmukil <ponmukil49@gmail.com>
2026-05-24 20:14:36 +0530

    Time: 61 ms (76.69%) | Memory: 86.8 MB (5.30%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,41 +1,33 @@
 class Pair{
     int key;
     int val;
-    Pair(int key, int val){
+    public Pair(int key, int val){
         this.key = key;
         this.val = val;
     }
 }
 class Solution {
     public int largestRectangleArea(int[] heights) {
-            int max = Integer.MIN_VALUE;
-            int i = 0;
-            Stack<Pair> st = new Stack<>();
-
-            while(i < heights.length){
-
-                while(!st.isEmpty() && st.peek().val > heights[i]){
-                    int k;
-                    Pair p = st.pop();
-                    if(st.isEmpty()) k = -1;
-                    else k = st.peek().key; 
-                    max = Integer.max(max, p.val * (i - k - 1));
-                }
-
-                st.push(new Pair(i, heights[i]));
-                i++;
-            }  
-
-            while(!st.isEmpty()){
-                int k;
+        Stack<Pair> st = new Stack<>();
+        int n = heights.length;
+        int maxArea = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++){
+            while(!st.isEmpty() && st.peek().val > heights[i]){
                 Pair p = st.pop();
+                int k;
                 if(st.isEmpty()) k = -1;
-                else k = st.peek().key; 
-                max = Integer.max(max, p.val * (heights.length - k -1));
-
+                else k = st.peek().key;
+                maxArea = Integer.max(maxArea, p.val * (i - k - 1));
             }
-            return max;
-
+            st.push(new Pair(i, heights[i]));
+        }
+        while(!st.isEmpty()){
+            Pair p = st.pop();
+            int k;
+            if(st.isEmpty()) k = -1;
+            else k = st.peek().key;
+            maxArea = Integer.max(maxArea, p.val * (n - k - 1));
+        }
+        return maxArea;
     }
-    
 }
