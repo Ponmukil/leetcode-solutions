622b28d91d67746e32cb222269655739f9b01d58
Ponmukil <ponmukil49@gmail.com>
2026-05-26 16:40:24 +0530

    Time: 73 ms (13.42%) | Memory: 158.2 MB (8.83%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,28 +1,26 @@
 class Pair{
     int key;
     int val;
-    Pair(int key, int val){
+    public Pair(int key, int val){
         this.key = key;
         this.val = val;
     }
 }
-
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int[] output = new int[nums.length - k + 1];
-        Deque<Pair> dq = new ArrayDeque<>();
+        int n = nums.length;
+        int[] out = new int[n-k+1];
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> b.val - a.val);
 
-        for(int i = 0;i < nums.length; i++){
-            if(!dq.isEmpty() && dq.peekFirst().key <= i-k ) dq.removeFirst();
-
-            while(!dq.isEmpty() && dq.peekLast().val <= nums[i]) dq.pollLast();
-
-            dq.addLast(new Pair(i, nums[i]));
-
-            if(i >= k-1) output[i+1-k] = dq.peekFirst().val;
+        for(int i = 0; i < n; i++){
+            pq.add(new Pair(i, nums[i]));
+            if(i >= k-1 ){
+                while(pq.peek().key <= i-k){
+                    pq.poll();
+                }
+                out[i+1-k] = pq.peek().val;
+            }
         }
-
-
-        return output;
+        return out;
     }
 }
