c82297af57943f398bf58d3c9a5c6b51e74bd42c
Ponmukil <ponmukil49@gmail.com>
2026-06-09 14:34:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (40.87%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -5,12 +5,7 @@ class Solution {
         n2 = goal.length();
         if(n1 != n2) return false;
 
-        for(int i = 0; i < n1; i++){
-            if(s.equals(goal)){
-                return true;
-            }
-            s = s.substring(1, n1) + s.charAt(0);
-        }
-        return false;
+        String doubled = s + s;
+        return doubled.contains(goal);
     }
 }
